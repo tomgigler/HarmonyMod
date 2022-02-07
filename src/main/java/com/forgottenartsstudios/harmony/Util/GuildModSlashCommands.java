ffe4428cddab1d6344a2cfd780c2618eb5eaa6bd
jnels <jnelsong1@gmail.com>
2022-02-06 22:33:54 -0500

    early commits

--- src/main/java/com/forgottenartsstudios/harmony/Util/GuildModSlashCommands.java
+++ src/main/java/com/forgottenartsstudios/harmony/Util/GuildModSlashCommands.java
@@ -15,6 +15,10 @@ import org.jetbrains.annotations.NotNull;
 import static com.forgottenartsstudios.harmony.Main.Harmony.out;
 
 import java.awt.Color;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class GuildModSlashCommands extends ListenerAdapter {
@@ -49,32 +53,64 @@ public class GuildModSlashCommands extends ListenerAdapter {
     public static void modControlPanel(SlashCommandInteractionEvent event){
         if (Objects.requireNonNull(event.getMember()).hasPermission(Permission.MODERATE_MEMBERS))
         {
+            //event.deferReply().queue();
             Moderation moderation = new Moderation();
             moderation.setName(event.getMember().getEffectiveName());
             moderation.setModPanelMember(Objects.requireNonNull(event.getOption("modmention")).getAsMember());
-            out.debug("Summon mod menu.");
+
             EmbedBuilder embed = new EmbedBuilder();
             embed.setColor(Color.CYAN);
             Member member = moderation.getModPanelMember();
-            assert member != null;
-            embed.setDescription("Mod panel for " + member.getEffectiveName());
+
+            Instant created = member.getTimeCreated().toInstant();
+            Instant join = member.getTimeJoined().toInstant();
+            Instant now = Instant.now();
+            Duration dCreated = Duration.between(created, now);
+            Duration dJoin = Duration.between(join, now);
+            String createdAge = format(dCreated);
+            String joinAge = format(dJoin);
+
+            embed.setAuthor(member.getEffectiveName());
+
+            embed.addField("id", member.getId(), true);
+            embed.addField("avatar", "[Link](" + member.getEffectiveAvatarUrl() + ")", true);
+            embed.addField("account created", member.getTimeCreated().format(DateTimeFormatter.RFC_1123_DATE_TIME), true);
+            embed.addField("account age", createdAge, true);
+            embed.addField("joined server at", member.getTimeJoined().format(DateTimeFormatter.RFC_1123_DATE_TIME), true);
+            embed.addField("join server age", joinAge, true);
+            embed.addField("status", member.getOnlineStatus().toString(), false);
+            embed.setFooter("id: " + event.getId());
+            embed.setThumbnail(member.getEffectiveAvatarUrl());
             MessageEmbed msgEmbed = embed.build();
             event.replyEmbeds(msgEmbed)
                     .addActionRow(
-                            Button.primary("mute", "   Mute    "), // Button with only a label
-                            Button.primary("timeout", "  Timeout  "), // Button with only a label
-                            Button.primary("nickname", " Nickname  "), // Button with only a label
-                            Button.primary("lookpup", "  Lookup   "), // Button with only a label
-                            Button.primary("ban", "    Ban   ")// Button with only a label
-                    )
-                    .addActionRow(
-                            Button.primary("kick", "   Kick    "), // Button with only a label
-                            Button.primary("warn", "   Warn    "), // Button with only a label
-                            Button.danger("slowmode", " Slowmode "), // Button with only a label
-                            Button.success("event", "   Event   "), // Button with only a label
-                            Button.primary("moderations", "Moderations")// Button with only a label
-                    )
-                    .queue();
+                            Button.primary("mute", "mute"), // Button with only a label
+                            Button.primary("timeout", "timeout"), // Button with only a label
+                            Button.primary("nickname", "nickname"), // Button with only a label
+                            Button.primary("lookpup", "lookup"), // Button with only a label
+                            Button.primary("ban", "ban")// Button with only a label
+                    ).addActionRow(
+                            Button.primary("kick", "kick"), // Button with only a label
+                            Button.primary("warn", "warn"), // Button with only a label
+                            Button.primary("moderations", "moderations")// Button with only a label
+                    ).queue();
         }
+    }
+    public static String format(Duration d) {
+        long years = (d.toDays() / 365);
+        d = d.minusDays(years * 365);
+        long days = d.toDays();
+        d = d.minusDays(days);
+        long hours = d.toHours();
+        d = d.minusHours(hours);
+        long minutes = d.toMinutes();
+        d = d.minusMinutes(minutes);
+        long seconds = d.getSeconds();
+        return
+                (years == 0?"":years+" years, ")+
+                        (days ==  0?"":days+" days, ")+
+                        (hours == 0?"":hours+" hours, ")+
+                        (minutes ==  0?"":minutes+" minutes, ")+
+                        (seconds == 0?"":seconds+" seconds");
     }
 }
